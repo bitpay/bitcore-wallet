581d38af8a57b7fa4155cec94c5be5fd84af4451
Chris Kleeschulte <chrisk@bitpay.com>
2017-03-08 01:12:44 -0500

    Finished signing script.

--- util/crypto-functions.js
+++ util/crypto-functions.js
@@ -10,49 +10,16 @@ exports.getPassphrase = function(callback) {
   ttyread('Enter passphrase: ', {silent: true}, callback);
 };
 
-exports.sha512KDF = function(passphrase, salt, derivationOptions, callback) {
-  if (!derivationOptions || derivationOptions.method !== 0 || !derivationOptions.rounds) {
-    return callback(new Error('SHA512 KDF method was called for, ' +
-      'yet the derivations options for it were not supplied.'));
-  }
-  var rounds =  derivationOptions.rounds || 1;
-  //if salt was sent in as a string, we will have to assume the default encoding type
-  if (!Buffer.isBuffer(salt)) {
-    salt = new Buffer(salt, 'hex');
-  }
-  var derivation = Buffer.concat([new Buffer(''), new Buffer(passphrase), salt]);
-  for(var i = 0; i < rounds; i++) {
-    derivation = crypto.createHash('sha512').update(derivation).digest();
-  }
-  callback(null, derivation);
-};
 
-exports.decryptPrivateKey = function(opts, callback) {
-  exports.decryptSecret(opts, function(err, masterKey) {
-    if(err) {
-      return callback(err);
-    }
-
-    opts.cipherText = opts.pkCipherText;
-    opts.key = masterKey;
-    opts.iv = bitcore.crypto.Hash.sha256sha256(new Buffer(opts.pubkey, 'hex'));
-    callback(null, exports.decrypt(opts));
-
-  });
-};
-
-exports.decryptSecret = function(opts, callback) {
-  exports.sha512KDF(opts.passphrase, opts.salt, opts.derivationOptions, function(err, hashedPassphrase) {
-    if (err) {
-      return callback(err);
-    }
+exports.decryptSecret = function(opts) {
+  var hashedPassphrase = exports.sha512KDF(opts.passphrase, opts.salt, opts.derivationOptions);
+  if (hashedPassphrase) {
     opts.key = hashedPassphrase;
-    callback(null, exports.decrypt(opts));
-  });
+  }
+  return exports.decrypt(opts);
 };
 
 exports.decrypt = function(opts) {
-
   if (!Buffer.isBuffer(opts.key)) {
     opts.key = new Buffer(opts.key, 'hex');
   }
@@ -72,17 +39,25 @@ exports.decrypt = function(opts) {
   return Buffer.concat([AESDecipher.update(cipherText), AESDecipher.final()]).toString('hex');
 };
 
-exports.confirm = function(question, callback) {
-  ttyread(question + ' (y/N): ', function(err, answer) {
-    if (err) {
-      return callback(err, false);
-    }
-    if (answer === 'y') {
-      return callback(null, true);
-    }
-    callback(null, false);
-  });
+exports.sha512KDF = function(passphrase, salt, derivationOptions) {
+  if (!derivationOptions || derivationOptions.method !== 0 || !derivationOptions.rounds) {
+    return;
+  }
+  var rounds =  derivationOptions.rounds || 1;
+
+  if (!Buffer.isBuffer(salt)) {
+    salt = new Buffer(salt, 'hex');
+  }
+  var derivation = Buffer.concat([new Buffer(''), new Buffer(passphrase), salt]);
+  for(var i = 0; i < rounds; i++) {
+    derivation = crypto.createHash('sha512').update(derivation).digest();
+  }
+  return derivation;
 };
 
+exports.diffTime = function(time) {
+  var diff = process.hrtime(time);
+  return (diff[0] * 1E9 + diff[1])/(1E9 * 1.0);
+}
 
 module.exports = exports;
