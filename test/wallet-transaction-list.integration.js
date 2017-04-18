11442fdb18b81d0c60dcf006b2216ecda50bf2f1
k <k@hungrysquirrel.kleetus.org>
2017-04-17 20:28:57 -0400

    Fixed jsonl parsing handler.

--- test/wallet-transaction-list.integration.js
+++ test/wallet-transaction-list.integration.js
@@ -143,6 +143,73 @@ describe('Wallet Transactions List', function() {
       });
 
     });
+
+    it('should handle a send and a receive tx', function(done) {
+      var expected = '{"txid":"5adc769843a5886d5bc7ac1ee48be9905de15f6ed6974b59c0175ac4aa449721","category":"receive","address":"1JsJffMQUi51v3q5R4DzGvhnPsHKMPSPdJ","outputIndex":0,"satoshis":607235}\n{"txid":"3f53213674d3f3906ce600b0cb18a7c59af890c4904daca4a9220982868f71ef","category":"send","address":"1AdNcCL4XMabbm6aXR3bK8LJhHPRFGH2iv","outputIndex":0,"satoshis":-321093}\n{"txid":"3f53213674d3f3906ce600b0cb18a7c59af890c4904daca4a9220982868f71ef","category":"fee","satoshis":-41520}\n';
+      var tx2 = new Transaction(txList.basicSendTx).toObject();
+      var tx1 = new Transaction(txList.basicReceiveTx).toObject();
+      tx2.inputs[0].inputSatoshis = 66692455;
+      tx1.inputs[0].inputSatoshis = 23693808;
+      var txStr = '';
+      [tx1, tx2].forEach(function(tx) {
+        txStr += JSON.stringify(tx).replace(/\n/g, '') + '\n';
+      });
+
+      this.proxy.on('request', function(req, res) {
+        res.statusCode = 200;
+        res.write(txStr, 'utf8', function(err) {
+          if(err) {
+            return done(err);
+          }
+          res.end();
+        });
+      });
+
+      exec(execString, function(err, stdout, stderr) {
+        expect(err).to.be.null;
+        stdout.should.equal(expected);
+        done();
+      });
+    });
+
+    it('should handle 2 large txs', function(done) {
+      var expected = '{"txid":"5adc769843a5886d5bc7ac1ee48be9905de15f6ed6974b59c0175ac4aa449721","category":"receive","address":"1JsJffMQUi51v3q5R4DzGvhnPsHKMPSPdJ","outputIndex":0,"satoshis":607235}\n{"txid":"3f53213674d3f3906ce600b0cb18a7c59af890c4904daca4a9220982868f71ef","category":"send","address":"1AdNcCL4XMabbm6aXR3bK8LJhHPRFGH2iv","outputIndex":0,"satoshis":-321093}\n{"txid":"3f53213674d3f3906ce600b0cb18a7c59af890c4904daca4a9220982868f71ef","category":"fee","satoshis":-13338532520}\n';
+      var tx2 = new Transaction(txList.basicSendTx).toObject();
+      var tx1 = new Transaction(txList.basicReceiveTx).toObject();
+      var input1 = tx1.inputs[0];
+      var input2 = tx2.inputs[0];
+      input2.inputSatoshis = 66692455;
+      input1.inputSatoshis = 23693808;
+      var txInputs1 = tx1.inputs;
+      var txInputs2 = tx2.inputs;
+      [txInputs1, txInputs2].forEach(function(txInputs) {
+        for(var i = 0; i < 200; i++) { // resulting tx should be 215kb
+          txInputs.push(txInputs[0]);
+        }
+      });
+      var txStr = '';
+      [tx1, tx2].forEach(function(tx) {
+        txStr += JSON.stringify(tx).replace(/\n/g, '') + '\n';
+      });
+
+
+      this.proxy.on('request', function(req, res) {
+        res.statusCode = 200;
+        res.write(txStr, 'utf8', function(err) {
+          if(err) {
+            return done(err);
+          }
+          res.end();
+        });
+      });
+
+      exec(execString, function(err, stdout, stderr) {
+        expect(err).to.be.null;
+        stdout.should.equal(expected);
+        done();
+      });
+
+    });
   });
 });
 
