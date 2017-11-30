3fd39bb056320d4a9fafdb44f3f78a3ca45b1daf
JDonadio <tanojaja@gmail.com>
2017-11-30 10:08:30 -0300

    add bch details

--- bin/cli-utils.js
+++ bin/cli-utils.js
@@ -274,6 +274,12 @@ Utils.renderAmount = function(amount) {
 
 
 Utils.UNITS = {
+  bch: {
+    name: 'bch',
+    toSatoshis: 100000000,
+    maxDecimals: 8,
+    minDecimals: 8,
+  },
   btc: {
     name: 'btc',
     toSatoshis: 100000000,
